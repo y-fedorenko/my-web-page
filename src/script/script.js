66bb3a6519b28cc768df94c9889dc52968da073a
Yaroslav Fedorenko <y.fedorenko@gmail.com>
2024-03-08 16:12:46 -0600

    about me section

--- src/script/script.js
+++ src/script/script.js
@@ -1,19 +1,6 @@
-/* - - - - - - - - - - - - - - - - - - - - - - - - - - - */
-/*                                                       */
-/*  Andre Specht (2024)                                  */
-/*  github.com/mrspecht                                  */
-/*                                                       */
-/* - - - - - - - - - - - - - - - - - - - - - - - - - - - */
-
 'use strict';
 
-// This app requires a server to handle import statements
-// and CORS issues
-import * as utils from './utils.js';
+function select(selector, parent = document) {
+  return parent.querySelector(selector);
+}
 
-
-/* - - - - - - - - - - - - - - - - - - - - - - - - - - - */
-/*  Organizer                                            */
-/* - - - - - - - - - - - - - - - - - - - - - - - - - - - */
-
-utils.print('Welcome');
