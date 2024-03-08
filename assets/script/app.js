3f598fc79c1fc525665d72e12383ce9fda9c6108
Yaroslav Fedorenko <y.fedorenko@gmail.com>
2024-03-08 09:26:28 -0600

    Project created

---
+++ assets/script/app.js
@@ -0,0 +1,19 @@
+/* - - - - - - - - - - - - - - - - - - - - - - - - - - - */
+/*                                                       */
+/*  Andre Specht (2024)                                  */
+/*  github.com/mrspecht                                  */
+/*                                                       */
+/* - - - - - - - - - - - - - - - - - - - - - - - - - - - */
+
+'use strict';
+
+// This app requires a server to handle import statements
+// and CORS issues
+import * as utils from './utils.js';
+
+
+/* - - - - - - - - - - - - - - - - - - - - - - - - - - - */
+/*  Organizer                                            */
+/* - - - - - - - - - - - - - - - - - - - - - - - - - - - */
+
+utils.print('Welcome');
